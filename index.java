3da1dc4519e383e5e4eb64b06c0c6952ea732ca4
TUXEDO FALCON <alerod0204@gmail.com>
2020-11-09 23:58:09 +0000

    Squares

--- index.java
+++ index.java
@@ -7,12 +7,31 @@ void draw() {
 	stroke(0);
 
   //start creating the lines
-
+line(200, 0, 200, 400);
+line(0, 200, 400, 200);
 
   //Fill a black color
+  fill(0);
   
-
   //Build your conditional statement 
-	
+if (mouseX <= 200 & mouseY <= 200){
+    rect(0, 0, 200, 200);
+
+}
+
+
+if (mouseX >= 200 & mouseY <= 200){
+    rect(200, 0, 200, 200);
+
+}
+
+if (mouseX <= 200 & mouseY >= 200){
+    rect(0, 200, 200, 200);
+
+}
+
+if (mouseX >= 200 & mouseY >= 200){
+    rect(200, 200, 200, 200);
+}
 
 }
